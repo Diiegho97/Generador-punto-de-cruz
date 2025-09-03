fd6d0906521c943716815da9f0c09d558bc82ac4
Diiegho97 <juandiegosdvsf@gmail.com>
2025-09-02 22:05:51 -0500

    Icono de flor en pestaña

--- app/views/patron.php
+++ app/views/patron.php
@@ -21,6 +21,8 @@ imagepng($canvas, $tempFile);
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Story+Script&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link rel="icon" type="image/svg+xml" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3E%3Cpath fill='%23e83e8c' d='M256 32c-17.7 0-32 14.3-32 32v64.1c-13.2-7.6-28.5-12.1-45-12.1-53 0-96 43-96 96 0 16.5 4.5 31.8 12.1 45H64c-17.7 0-32 14.3-32 32s14.3 32 32 32h64.1c-7.6 13.2-12.1 28.5-12.1 45 0 53 43 96 96 96 16.5 0 31.8-4.5 45-12.1V448c0 17.7 14.3 32 32 32s32-14.3 32-32v-64.1c13.2 7.6 28.5 12.1 45 12.1 53 0 96-43 96-96 0-16.5-4.5-31.8-12.1-45H448c17.7 0 32-14.3 32-32s-14.3-32-32-32h-64.1c7.6-13.2 12.1-28.5 12.1-45 0-53-43-96-96-96-16.5 0-31.8 4.5-45 12.1V64c0-17.7-14.3-32-32-32z'/%3E%3C/svg%3E">
 </head>
 <body style="background: url('uploads/telallo_paris.jpg') center center / cover no-repeat fixed; image-rendering: auto; background-attachment: fixed; background-size: cover; background-position: center center; background-repeat: no-repeat;" class="bg-light">
 <div class="container py-5">
